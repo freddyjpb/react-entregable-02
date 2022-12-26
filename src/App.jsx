24e261d7feee616f049d0b8b40c0af9f1de4e3a7
freddyjpb <114787879+freddyjpb@users.noreply.github.com>
2022-12-26 09:01:47 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
+import loaderLogo from './assets/cdlogo.png'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showElement, setShowElement] = useState(true);
+
+  useEffect(() => {
+    setTimeout(function () { setShowElement( false ); }, 3000 );
+  }, []);
+  
 
   return (
     <div className="App">
+      
+      
+      <div className="loader1"> 
+        {showElement ? ( 
+          <div className="loader2" style={{ opacity: showElement ? 1 : 0 }} >
+            I'm here and i will be gone
+            <img src={loaderLogo} className="logo react" alt="React logo" />
+          </div>) 
+        : 
+          (<div>esto no va aqui</div>)}{" "}
+      </div>
+
+      
+      
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
