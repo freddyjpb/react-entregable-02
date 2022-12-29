37be2651c2d381986432d3a16b9284259bcbfe53
freddyjpb <114787879+freddyjpb@users.noreply.github.com>
2022-12-29 10:06:28 -0600

    Add files via upload

---
+++ src/components/src/App.jsx
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import loaderLogo from './assets/cdlogo.png'
+import WeatherCard from './components/WeatherCard';
+
+import './App.css'
+
+function App() {
+  const [ showElement, setShowElement ] = useState( true );
+  const [showDarkMode, setShowDarkMode] = useState(true);
+  const [ theme, setTheme ] = useState ( 'light' );
+  const [ coords, setCoords ] = useState();
+  const [ weather, setWeather ] = useState({});
+
+  const handleClickDarkMode = () => {
+    setShowDarkMode(!showDarkMode);
+    if ( showDarkMode ) {
+      setTheme( 'dark' );
+    } else {
+      setTheme( 'light' );
+    }
+  }
+
+  const success = pos => { 
+    setCoords ({
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    });
+  };
+
+  //console.log( coords );
+
+  useEffect(() => {
+    setTimeout(function () { setShowElement( false ); }, 10000 );
+    navigator.geolocation.getCurrentPosition( success );
+  }, []);
+  
+  useEffect(() => {
+    document.body.className = theme;
+    console.log( theme );
+  }, [ theme ]);
+
+  useEffect(() => {
+    if ( coords ) {
+      const weatherApiKey = '914c1ad469eef39db3b797bc89801e76';
+      const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${weatherApiKey}&units=metric`;
+      axios
+      .get(weatherApiUrl)
+      .then((res) => setWeather(res.data))
+      .catch((err) => console.log(err));
+    }
+  }, [ coords ]);
+
+  //console.log( weather );
+
+  return (
+    <div className={ `App ${theme }`}>
+
+       <div className="App__loader--container"> 
+        {showElement ? ( 
+          <div className="App__loader--image" style={{ opacity: showElement ? 1 : 0 }} >
+            <img src={loaderLogo}/>
+          </div>) 
+        : 
+          (<div></div>)}{" "}
+      </div>
+
+      <div>
+        <div className={ `App__title ${theme}`}>
+          <h1 className={ `h1-autoresize--${theme}`}>Weather App</h1>
+          <div>
+            <button onClick={handleClickDarkMode}>
+              {showDarkMode ? (
+                <i className='bx bx-moon'></i>
+              ) : (
+                <i className='bx bx-sun'></i>
+              )}
+            </button>
+            <button><i className='bx bx-menu'></i></button>
+          </div>
+        </div>
+        <WeatherCard weather={ weather }/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/src/components/WeatherCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const WeatherCard = ({ weather }) => {
+    //console.log( weather );
+    const weatherIconName = weather.weather?.[0].icon;
+
+return (
+    <div className='Temp'>
+         <h2>{ weather.name }</h2>
+        <h1>{ weather.sys?.country }</h1>
+        <h2>GMT{ weather.timezone / 3600 }</h2>
+        <p>Latitud:{ weather.coord?.lat }</p>
+        <p>Longitud:{ weather.coord?.lon }</p>
+        <p>Temperatura:{ weather.main?.temp }</p>
+        <img src={weatherIconName &&  `http://openweathermap.org/img/wn/${weatherIconName}@4x.png`} alt="" />
+        <footer><button>Degrees C/F</button></footer>
+    </div>
+)
+}
+
+export default WeatherCard;
