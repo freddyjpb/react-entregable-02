5c53685623b26b80b3a2843372cb92daeb544cdb
freddyjpb <114787879+freddyjpb@users.noreply.github.com>
2022-12-29 12:58:26 -0600

    Add files via upload

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useEffect} from 'react'
-
+import { useEffect } from 'react';
 import './WeatherCard.css';
 
 const WeatherCard = ({ weather, theme }) => {
@@ -11,20 +10,20 @@ const WeatherCard = ({ weather, theme }) => {
     useEffect(() => {
         document.body.className = theme;
         //console.log( theme );
-      }, [ theme ]);
+    }, [theme]);
 
-return (
-    <div className={ `Weather__card ${theme }`}>
-        <h2>{ weather.name }</h2>
-        <h1>{ weather.sys?.country }</h1>
-        <h2>GMT{ weather.timezone / 3600 }</h2>
-        <p>Latitud:{ weather.coord?.lat }</p>
-        <p>Longitud:{ weather.coord?.lon }</p>
-        <p>Temperatura:{ weather.main?.temp }</p>
-        <img src={weatherIconName &&  `http://openweathermap.org/img/wn/${weatherIconName}@4x.png`} alt="" />
-        <footer><button>Degrees C/F</button></footer>
-    </div>
-)
+    return (
+        <div className={`Weather__card ${theme}`}>
+            <h2 className={ `h2-autoresize--${theme}`}>{weather.name}</h2>
+            <h2 className={ `h2-autoresize--${theme}`}>{weather.sys?.country}</h2>
+            <h1 className={ `h1-autoresize--${theme}`}>GMT{weather.timezone / 3600}</h1>
+            <p>Latitud:{weather.coord?.lat}</p>
+            <p>Longitud:{weather.coord?.lon}</p>
+            <p>Temperatura:{weather.main?.temp}</p>
+            <img src={weatherIconName && `http://openweathermap.org/img/wn/${weatherIconName}@4x.png`} alt="" />
+            <footer><button>Degrees C/F</button></footer>
+        </div>
+    )
 }
 
 export default WeatherCard;
